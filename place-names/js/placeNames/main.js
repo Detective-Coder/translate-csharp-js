7812b9aa0f96cbf64f6e022a87da175859d38031
Detective-Coder <aaronfrankenfield@gmail.com>
2021-04-14 11:13:38 -0400

    updated placeNames for js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,18 @@
 // Put your code here
+console.log("All Place Names")
+
+const placeNames = ["Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+for (let i = 0; i < placeNames.length; i++) {
+  console.log(placeNames[i])
+}
+
+console.log("'The' Place Names")
+
+let places = placeNames.filter(placeName => placeName.startsWith("The"))
+
+ for (let j = 0; j < places.length; j++) {
+   console.log(places[j])
+ }
+
